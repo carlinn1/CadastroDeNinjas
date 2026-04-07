1ca3d49c2995ab8565ba4e20232a8cf130092b0f
carlinn1 <carlos_-425@hotmail.com>
2026-04-07 15:00:52 -0300

    Feat: Adicionado o Swagger e comentado via documentação do swagger!

--- src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.java10x.CadastroDeNinjas.dto.NinjaDTO;
 import dev.java10x.CadastroDeNinjas.service.NinjaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping
@@ -33,11 +36,21 @@ public class NinjaController {
     private NinjaService ninjaService;
 
     @GetMapping("/listar")
+    @Operation(summary = "Listar todos os ninjas", description = "Retorna uma lista de todos os ninjas cadastrados no sistema.")   
+    @ApiResponse(responseCode = "200", description = "Lista de ninjas retornada com sucesso") 
+    @ApiResponse(responseCode = "500", description = "Erro interno ao tentar listar os ninjas")
+    @Parameter(name = "Nenhum parâmetro necessário para listar os ninjas", description = "Este endpoint não requer parâmetros de entrada", required = false) 
+    
     public List<NinjaDTO> getNinjas() {
         return ninjaService.getNinjas();
     }
 
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Obter ninja por ID", description = "Retorna os detalhes de um ninja específico com base no seu ID.")    
+    @ApiResponse(responseCode = "200", description = "Ninja retornado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Ninja não encontrado com o ID fornecido")
+    @Parameter(name = "id", description = "ID do ninja a ser obtido", required = true)
+
     public NinjaDTO getNinjaById(@PathVariable Long id) {
 
         return ninjaService.getNinjaById(id);
@@ -45,6 +58,11 @@ public class NinjaController {
 
     //Adicionar ninjas
     @PostMapping("/criar")
+    @Operation(summary = "Criar um novo ninja", description = "Adiciona um novo ninja ao sistema com os detalhes fornecidos.")  
+    @ApiResponse(responseCode = "201", description = "Ninja criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Requisição inválida para criar ninja")
+    @Parameter(name = "ninja", description = "Objeto NinjaDTO contendo os detalhes do ninja a ser criado", required = true)
+
     public ResponseEntity<String> addNinja(@RequestBody NinjaDTO ninja) {
         NinjaDTO createdNinja = ninjaService.addNinja(ninja);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -52,6 +70,12 @@ public class NinjaController {
     }
 
     @PutMapping("/atualizar/{id}")
+    @Operation(summary = "Atualizar um ninja", description = "Atualiza os detalhes de um ninja específico com base no seu ID.")  
+    @ApiResponse(responseCode = "200", description = "Ninja atualizado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Ninja não encontrado para atualizar")
+    @Parameter(name = "id", description = "ID do ninja a ser atualizado", required = true)
+    @Parameter(name = "ninja", description = "Objeto NinjaDTO contendo os detalhes do ninja a ser atualizado", required = true)
+
         public ResponseEntity<String> updateNinja(@PathVariable Long id, @RequestBody NinjaDTO ninja) {
             NinjaDTO updatedNinja = ninjaService.updateNinja(id, ninja);
             if (updatedNinja != null) {
@@ -62,6 +86,11 @@ public class NinjaController {
     }
 
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deletar um ninja", description = "Remove um ninja específico do sistema com base no seu ID.")  
+    @ApiResponse(responseCode = "200", description = "Ninja deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Ninja não encontrado para deletar")
+    @Parameter(name = "id", description = "ID do ninja a ser deletado", required = true)
+    
     public ResponseEntity<String> deleteNinja(@PathVariable Long id) {
         
         if (ninjaService.deleteNinja(id)) {
